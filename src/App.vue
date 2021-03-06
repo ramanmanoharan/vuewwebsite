b4cfea81ca074069ceeaab51384ab90750485f5d
ramanmanoharan <58263724+ramanmanoharan@users.noreply.github.com>
2021-03-07 00:16:15 +0530

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@
    <nav class="w3-sidebar w3-red w3-collapse w3-top w3-large w3-padding" style="z-index:3;width:300px;font-weight:bold;" id="mySidebar"><br>
   <a href="javascript:void(0)" onclick="w3_close()" class="w3-button w3-hide-large w3-display-topleft" style="width:100%;font-size:22px">Close Menu</a>
   <div class="w3-container">
-    <img src="./assets/logomain.png">
+    <img src="./assets/logo.png">
     <h3 class="w3-padding-64"><b>IT Services</b></h3>
   </div>
   <div class="w3-bar-block">
