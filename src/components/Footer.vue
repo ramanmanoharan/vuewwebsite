123171937da321740e01155863c7ae153830fcd0
ramanmanoharan <58263724+ramanmanoharan@users.noreply.github.com>
2021-03-07 00:13:38 +0530

    Update Footer.vue

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,11 +1,11 @@
 
 <template>
-	<div class="w3-light-grey w3-container w3-padding-32 foot" style="margin-top:75px;padding-right:58px"><p class="w3-right">Powered by <a href="https://redbackstudios.in" title="W3.CSS" target="_blank" class="w3-hover-opacity">{{message}}</a></p></div>
+	<div class="w3-light-grey w3-container w3-padding-32 foot" style="margin-top:75px;padding-right:58px"><p class="w3-right">Powered by <a href="https://ramanvellore2020.pythonanywhere.com" title="W3.CSS" target="_blank" class="w3-hover-opacity">{{message}}</a></p></div>
 </template>
 <script>
 export default {
   name: 'Footer',
-  data(){return{ message:"Redback" } },
+  data(){return{ message:"Ram" } },
   props: {
     msg: String,
     data: String
